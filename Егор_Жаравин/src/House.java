4c3cabee6dbcad30637db644bd267fcd0f3a5b98
Yegorka <egorzharavin6@gmail.com>
2021-10-24 22:00:27 +0300

    Your Name. My first Class

---
+++ Егор_Жаравин/src/House.java
@@ -0,0 +1,12 @@
+public class House {
+    int a;
+    int b;
+    public static void main (String[] args)
+    {
+        System.out.print(1);
+    }
+    public static double pi ()
+    {
+        return 3.14;
+    }
+}
