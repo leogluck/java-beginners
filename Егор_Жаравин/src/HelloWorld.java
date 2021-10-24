f877a38598bfebec63c69d1ecfd21c323aaa30e7
Yegorka <egorzharavin6@gmail.com>
2021-10-24 22:00:27 +0300

    Your Name. My first Class

--- Егор_Жаравин/src/HelloWorld.java
+++ Егор_Жаравин/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Привет как дела?");
+        System.out.println("У меня хорошо!");
+        System.out.println("Давай танцевать?");
     }
 }
