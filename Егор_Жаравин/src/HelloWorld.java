51d36c892b21333f98c33933dd6ba2cd13e5025a
Leogluck <leogluck@gmail.com>
2021-11-01 10:17:50 +0300

    ЕгорЖаравин (#22)
    * Your Name. My first Class
    
    * Your Name. My first Class
    
    * Your Name. My first Class
    
    Co-authored-by: Yegorka <egorzharavin6@gmail.com>

---
+++ Егор_Жаравин/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Привет как дела?");
+        System.out.println("У меня хорошо!");
+        System.out.println("Давай танцевать?");
+    }
+}
