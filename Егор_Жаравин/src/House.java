e1e929651c661d6f085c8bc4f1602e044a7329d1
Yegorka <egorzharavin6@gmail.com>
2021-11-02 16:35:48 +0300

    Your Name. My first Class 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

--- Егор_Жаравин/src/House.java
+++ Егор_Жаравин/src/House.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class House {
-    int a;
-    int b;
-    public static void main (String[] args)
-    {
-        System.out.print(1);
-    }
-    public static double pi ()
-    {
-        return 3.14;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число x:");
+        int x = in.nextInt();
+        System.out.printf("Ваш номер: %d", x);
+        in.close();
+
+        Scanner out = new Scanner(System.in  );
+        System .out.print("Введите число y:");
+        int y=in.nextInt();
+        System.out.printf("Ваш номер: %d", y);
+        in.close();
+        int d=x+y;
+        System.out.println(d);
     }
 }
