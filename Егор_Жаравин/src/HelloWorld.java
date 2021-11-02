6bd8a67d11be0808c4823fc092f6c36819d1c50b
Yegorka <egorzharavin6@gmail.com>
2021-11-02 16:31:56 +0300

    Your Name. My first Class

--- Егор_Жаравин/src/HelloWorld.java
+++ Егор_Жаравин/src/HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld {
         System.out.println("Привет как дела?");
         System.out.println("У меня хорошо!");
         System.out.println("Давай танцевать?");
+            int a = 10;
+            int b = 7;
+            int c = a + b;
+            int d = 10 - b;
+            int x=a*d-b+c;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(x);
+
     }
 }
