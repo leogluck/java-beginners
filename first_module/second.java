fb28fc5d31b471b5a3749c49739fac7d98f8c962
Danya <danylo.yakovenko@gmail.com>
2021-11-06 19:19:57 +0200

    Данил Яковенко. 2

---
+++ first_module/second.java
@@ -0,0 +1,9 @@
+public class second {
+    public static void main(String[] args){
+        double a = Math.random() * 10;
+        double b = Math.random() * 10;
+        double c = Math.random() * 10;
+
+        System.out.println (a + b * c / 2);
+    }
+}
