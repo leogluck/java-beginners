7aa8be678f5c4e8574fc5080369df81cc72608e6
PushkarovMYu <93119845+PushkarovMYu@users.noreply.github.com>
2021-11-08 21:46:02 +0200

    Homework first task (#31)

---
+++ pushkarov_misha/src/BiggestNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input a number 'a', 'b' and 'c': ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = Math.max(Math.max(a, b), c);
+
+        System.out.println(d);
+    }
+}
