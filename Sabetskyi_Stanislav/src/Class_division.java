a1c335807ca71ab565d55376d276a6b3c7710548
helka <helkat0212@gmail.com>
2021-11-09 20:39:56 +0200

    Stanislav. Homework

--- Sabetskyi_Stanislav/src/Class_division.java
+++ Sabetskyi_Stanislav/src/Class_division.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Class_Class {
+public class Class_division {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input two numbers:");
