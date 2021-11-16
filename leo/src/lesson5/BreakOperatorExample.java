38642bca17f460c22d5920619fada2ab157e2f92
leo <leogluck@gmail.com>
2021-11-16 21:28:02 +0200

    LS-5: Added example classes for `continue` and `break` operators

---
+++ leo/src/lesson5/BreakOperatorExample.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class BreakOperatorExample {
+    public static void main(String[] args) {
+        int age = 10;
+
+        while (true) {
+            System.out.println("Your age is: " + age);
+            if (age >= 18) {
+                break;
+            }
+            age++;
+        }
+
+        System.out.println("You can come inside");
+    }
+}
