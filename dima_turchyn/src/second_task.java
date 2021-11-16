832ece1b017afaeacc0656459df51a04e15d5b28
dim4ukt <dim4ukt@gmail.com>
2021-11-16 21:44:44 +0200

    save

---
+++ dima_turchyn/src/second_task.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class second_task {
+    Scanner in = new Scanner(System.in);
+        System.out.print("Input a: ");
+    int a_numder = in.nextInt();
+        System.out.print("Input b: ");
+    int b_numder = in.nextInt();
+        System.out.print("Input c: ");
+    int c_numder = in.nextInt();
+}
