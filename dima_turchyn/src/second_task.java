f9a8376bb1284c3c3b77b7bfbb3eb4cda2733b31
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2021-11-19 14:01:57 +0200

    Update second_task.java

--- dima_turchyn/src/second_task.java
+++ dima_turchyn/src/second_task.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class second_task {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
 
         Scanner in = new Scanner(System.in);
         System.out.printf("Enter a: ");
