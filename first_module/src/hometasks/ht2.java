fd98f754f1526d91cafd152d91ccd4769900c0ca
Danya <danylo.yakovenko@gmail.com>
2021-11-19 15:13:27 +0200

    Данил Яковенко. 2 дз

--- first_module/src/hometasks/ht2.java
+++ first_module/src/hometasks/ht2.java
@@ -1,7 +1,8 @@
 package hometasks;
+
 import java.util.Scanner;
 
-public class ht3 {
+public class ht2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a: ");
@@ -14,10 +15,10 @@ public class ht3 {
         int ans_2 = ((a_number * a_number) + (b_number * b_number)) / 2;
         int ans_3 = ((a_number + b_number) / 12 * c_number % 4 + b_number);
         int ans_4 = (a_number - b_number * c_number) / (a_number + b_number) % c_number;
-
         System.out.println("Ответ номер 1 - " + ans_1);
         System.out.println("Ответ номер 2 - " + ans_2);
         System.out.println("Ответ номер 3 - " + ans_3);
         System.out.println("Ответ номер 4 - " + ans_4);
     }
+
 }
