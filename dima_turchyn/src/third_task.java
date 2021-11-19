074908c8ecbf06c56718815a8e3283e5471997f7
dim4ukt <dim4ukt@gmail.com>
2021-11-19 14:10:41 +0200

    3rd task Done

---
+++ dima_turchyn/src/third_task.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class third_task {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Enter number 1: ");
+        int number_1 = in.nextInt();
+        System.out.printf("Enter number 2: ");
+        int number_2 = in.nextInt();
+        System.out.printf("Enter number 3 : ");
+        int number_3 = in.nextInt();
+        if (number_1>number_2 & number_1>number_3){
+            System.out.println(number_1+" is the biggest");
+        }
+        else if (number_2>number_1 & number_2>number_3){
+            System.out.println(number_2+" is the biggest");
+        }
+        else if (number_3>number_2 & number_3>number_1){
+            System.out.println(number_3+" is the biggest");
+        }
+    }
+}
