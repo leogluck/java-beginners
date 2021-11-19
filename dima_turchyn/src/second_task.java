e66eb96b725fabe6395aa011363c0a6fc747c813
dim4ukt <dim4ukt@gmail.com>
2021-11-19 13:44:56 +0200

    Done

--- dima_turchyn/src/second_task.java
+++ dima_turchyn/src/second_task.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
 public class second_task {
-    Scanner in = new Scanner(System.in);
-        System.out.print("Input a: ");
-    int a_numder = in.nextInt();
-        System.out.print("Input b: ");
-    int b_numder = in.nextInt();
-        System.out.print("Input c: ");
-    int c_numder = in.nextInt();
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Enter a: ");
+        int a = in.nextInt();
+        System.out.printf("Enter b: ");
+        int b = in.nextInt();
+        System.out.printf("Enter c: ");
+        int c = in.nextInt();
+
+        System.out.println((a + (b * c)) / 2);
+        System.out.println(((a * a) + (b * b)) / 2);
+        System.out.println((a + b) / 12 * c % 4 + b);
+        System.out.println((a - b * c) / (a + b) % c);
+
+
+    }
 }
