472131b689e5f78b2f713e42a0f91f890b3eca5a
Leogluck <leogluck@gmail.com>
2021-11-21 17:33:23 +0200

    Update and rename second_task.java to SecondTask.java

--- dima_turchyn/src/SecondTask.java
+++ dima_turchyn/src/SecondTask.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class second_task {
+public class SecondTask {
     public static void main(String[] args) {
-        
-
         Scanner in = new Scanner(System.in);
+        
         System.out.printf("Enter a: ");
         int a = in.nextInt();
+        
         System.out.printf("Enter b: ");
         int b = in.nextInt();
+        
         System.out.printf("Enter c: ");
         int c = in.nextInt();
 
@@ -16,7 +17,5 @@ public class second_task {
         System.out.println(((a * a) + (b * b)) / 2);
         System.out.println((a + b) / 12 * c % 4 + b);
         System.out.println((a - b * c) / (a + b) % c);
-
-
     }
 }
