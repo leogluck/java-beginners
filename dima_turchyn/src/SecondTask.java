82104244d374f674173b17dcd634e8169ff9f84d
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2021-11-21 17:34:10 +0200

    Task 2 dima thurchyn (#46)
    * save
    
    * Done
    
    * Update second_task.java
    
    * Update and rename second_task.java to SecondTask.java
    
    Co-authored-by: Leogluck <leogluck@gmail.com>

---
+++ dima_turchyn/src/SecondTask.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.printf("Enter a: ");
+        int a = in.nextInt();
+        
+        System.out.printf("Enter b: ");
+        int b = in.nextInt();
+        
+        System.out.printf("Enter c: ");
+        int c = in.nextInt();
+
+        System.out.println((a + (b * c)) / 2);
+        System.out.println(((a * a) + (b * b)) / 2);
+        System.out.println((a + b) / 12 * c % 4 + b);
+        System.out.println((a - b * c) / (a + b) % c);
+    }
+}
