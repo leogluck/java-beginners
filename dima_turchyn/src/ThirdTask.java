80614fc6c1fd48a41341c19d5d59a4f37fea90c0
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2021-11-28 15:31:50 +0200

    3rd task Done (#47)
    * 3rd task Done
    
    * bugs fixed
    
    * bugs fixed
    
    * done
    
    * Delete ant.xml
    
    * Delete runConfigurations.xml
    
    * Delete vcs.xml
    
    * Delete CostOfThing.java
    
    * Delete DOOM.java
    
    * Delete MathClass.java
    
    * Delete Max_input.java
    
    * Delete Program.java
    
    * Delete TelephoneCall.java
    
    Co-authored-by: Leogluck <leogluck@gmail.com>

---
+++ dima_turchyn/src/ThirdTask.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ThirdTask {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Enter number 1: ");
+        int number1 = in.nextInt();
+        System.out.printf("Enter number 2: ");
+        int number2 = in.nextInt();
+        System.out.printf("Enter number 3 : ");
+        int number3 = in.nextInt();
+        if (number1 > number2 & number1 > number3 & number1 != number2 & number1 != number3) {
+            System.out.println(number1 + " is the biggest");
+        } else if (number2 > number1 & number2 > number3 & number2 != number3 & number2 != number1) {
+            System.out.println(number2 + " is the biggest");
+        } else if (number3 > number2 & number3 > number1 & number3 != number2 & number3 != number1) {
+            System.out.println(number3 + " is the biggest");
+        }
+    }
+}
