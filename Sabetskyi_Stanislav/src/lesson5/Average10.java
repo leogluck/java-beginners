a0caf4d3136840cc239b0b7bcde4fc7057ca5f3b
helka <helkat0212@gmail.com>
2021-12-01 19:47:40 +0200

    Stanislav SI. Homework 5

---
+++ Sabetskyi_Stanislav/src/lesson5/Average10.java
@@ -0,0 +1,37 @@
+package lesson5;
+
+public class Average10 {
+    public static void main(String[] args) {
+        int[] numbs = new int[10];
+        {
+            numbs[0] = 12;
+            numbs[1] = 7;
+            numbs[2] = 400;
+            numbs[3] = 10;
+            numbs[4] = 5;
+            numbs[5] = 25;
+            numbs[6] = 14;
+            numbs[7] = 100;
+            numbs[8] = 1;
+            numbs[9] = 500;
+        }
+        int sum = 0;
+        int x=-1;
+        boolean count=true;
+        while (count==true){
+            x++;
+            sum=numbs[x]+sum;
+            System.out.println("Sum is "+sum);
+            if (x> numbs.length-2){
+                count=false;
+            }
+            else {
+                continue;
+            }
+        }
+        int average;
+        average=sum/ numbs.length;
+        System.out.println("Average number is "+average);
+
+    }
+}
---
+++ Sabetskyi_Stanislav/src/lesson5/ChessCort.java
@@ -0,0 +1,15 @@
+package lesson5;
+
+public class ChessCort {
+    public static void main(String[] args) {
+        String[] corts = new String[2];
+        {
+            corts[0] = "| W | B | W | B | W | B | W | B |";
+            corts[1] = "| B | W | B | W | B | W | B | W |";
+        }
+        for (int i=0; i<4;i++){
+            System.out.println(corts[0]);
+            System.out.println(corts[1]);
+        }
+    }
+}
