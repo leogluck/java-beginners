03e6a9711e0747881545dc4e8a0b51a317d7309e
Danya <danylo.yakovenko@gmail.com>
2021-12-07 22:02:16 +0200

    Данил Яковенко. 3 дз

--- src/danil_yakovenko/hometask3/HT3.java
+++ src/danil_yakovenko/hometask3/HT3.java
@@ -11,9 +11,9 @@ public class HT3 {
         int number2 = in.nextInt();
         System.out.printf("Введите число 3 : ");
         int number3 = in.nextInt();
-        if (number1 > number2 && number1 > number3) {
+        if (number1 > number2 && number1 >= number3) {
             System.out.println(number1 + " большее");
-        } else if (number2 > number1 && number2 > number3) {
+        } else if (number2 > number1 && number2 >= number3) {
             System.out.println(number2 + " большее");
         } else if (number3 > number2 && number3 > number1) {
             System.out.println(number3 + " большее");
