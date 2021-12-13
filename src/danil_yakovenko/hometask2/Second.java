57cf4a5f3b509026e3009958696f5a4646ec71ce
Leogluck <leogluck@gmail.com>
2021-12-14 00:11:48 +0300

    First name family name (#65)
    * Данил Яковенко. Му Фірст Лласс
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Delete .gitignore
    
    * Delete ht2.java
    
    * Delete ht4.java
    
    * Delete ht3.java
    
    Co-authored-by: Danya <danylo.yakovenko@gmail.com>

---
+++ src/danil_yakovenko/hometask2/Second.java
@@ -0,0 +1,11 @@
+package danil_yakovenko.hometask2;
+
+public class Second {
+    public static void main(String[] args){
+        double a = Math.random() * 10;
+        double b = Math.random() * 10;
+        double c = Math.random() * 10;
+
+        System.out.println (a + b * c / 2);
+    }
+}
