5c4b9acc21eea10aba9e689cbf760a34037ccc29
egor muckhin <megor2810@gmail.com>
2021-12-13 21:54:29 +0200

    muchin egor

---
+++ src/egor_muchin/hometask1/SecondTask.java
@@ -0,0 +1,21 @@
+package egor_muchin.hometask1;
+
+import java.util.Scanner;
+
+public class SecondTask {
+ public static void main(String[] args) {
+     int b;
+     int c;
+     int a;
+     try (Scanner in = new Scanner(System.in)) {
+         System.out.print("Enter a:");
+         a = in.nextInt();
+         System.out.print("Enter b:");
+         b = in.nextInt();
+         System.out.print("Enter c: ");
+         c = in.nextInt();
+     }
+     System.out.println((a + (b * c)) / 2);
+     System.out.println(((a * a) + (b * b)) / 2);
+     System.out.println((a + b) / 12 * c % 4 + b);
+     System.out.println((a - b * c) / (a + b) % c);
