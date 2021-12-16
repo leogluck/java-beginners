82c981946f4165994f509fb4ac13e415e17a8b55
leo <leogluck@gmail.com>
2021-12-16 14:51:24 +0200

    Home task 4

---
+++ src/sabetskyi_stanislav/hometask4/DivisionOn7.java
@@ -0,0 +1,20 @@
+package sabetskyi_stanislav.hometask4;
+
+public class DivisionOn7 {
+    public static void main(String[] args) {
+        int number =35;
+        boolean ans=true;
+        while(ans==true){
+            number++;
+            if (number%7==1||number%7==5||number%7==2){
+                System.out.println(number);
+            }
+            else if (number==87){
+                ans=false;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
---
+++ src/sabetskyi_stanislav/hometask4/EasyOrDifficult.java
@@ -0,0 +1,31 @@
+package sabetskyi_stanislav.hometask4;
+
+import java.util.Scanner;
+
+public class EasyOrDifficult {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input any number:");
+        int number = in.nextInt();
+        boolean ans = true;
+        int num = 2;
+        while (ans == true) {
+            num++;
+            if (number % num != 0) {
+                continue;
+            } else if (num == number) {
+                System.out.println(number + " is easy number");
+                break;
+            } else if (number % num == 0) {
+                System.out.println(number + " is difficult number");
+                break;
+            }
+        }
+    }
+
+
+}
+
+
+
+
