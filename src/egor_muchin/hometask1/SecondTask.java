180986fabf3723167b0eab5eb77dc3a4fb1ab4d9
Leogluck <leogluck@gmail.com>
2021-12-21 21:04:44 +0300

    Deleted some unnecessary code (#64)

--- src/egor_muchin/hometask1/SecondTask.java
+++ src/egor_muchin/hometask1/SecondTask.java
@@ -2,19 +2,21 @@ package egor_muchin.hometask1;
 
 import java.util.Scanner;
 
-public class SecondTask<a, b, c> {
+public class SecondTask {
     public static void main(String[] args) {
         int b;
         int c;
         int a;
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter a:");
-            a = in.nextInt();
-            System.out.print("Enter b:");
-            b = in.nextInt();
-            System.out.print("Enter c: ");
-            c = in.nextInt();
-        }
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a:");
+        a = in.nextInt();
+        System.out.print("Enter b:");
+        b = in.nextInt();
+        System.out.print("Enter c: ");
+        c = in.nextInt();
+
         System.out.println((a + (b * c)) / 2);
         System.out.println(((a * a) + (b * b)) / 2);
         System.out.println((a + b) / 12 * c % 4 + b);
