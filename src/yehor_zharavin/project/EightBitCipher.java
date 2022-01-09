dcc0442338d453cf234ee61758a322abd8e4135c
Yegorka <zharavin2006@gmail.com>
2022-01-09 17:48:48 +0300

    Project

---
+++ src/yehor_zharavin/project/EightBitCipher.java
@@ -0,0 +1,11 @@
+package yehor_zharavin.project;
+
+public class EightBitCipher {
+    public static void main(String[] args) {
+        int a=3;
+        int b=8;
+       if((a == 3) ^(b == 8))System.out.println("GG");
+        System.out.println("a ^ b = " + (a ^ b));
+
+    }
+}
