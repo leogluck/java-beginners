ea3b3eb96331a1168e77a04dacef0bbcb6048432
leogluck <leogluck@gmail.com>
2022-01-09 17:22:45 +0200

    Moved some code to example class

--- src/yehor_zharavin/project/EightBitCipher.java
+++ src/yehor_zharavin/project/EightBitCipher.java
@@ -2,10 +2,6 @@ package yehor_zharavin.project;
 
 public class EightBitCipher {
     public static void main(String[] args) {
-        int a=3;
-        int b=8;
-       if((a == 3) ^(b == 8))System.out.println("GG");
-        System.out.println("a ^ b = " + (a ^ b));
 
     }
 }
