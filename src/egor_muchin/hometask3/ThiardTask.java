c1b91314a44aec76258136a158a0b89dadda49c2
egor muckhin <megor2810@gmail.com>
2022-01-11 22:31:36 +0200

    ExtryRaner

---
+++ src/egor_muchin/hometask3/ThiardTask.java
@@ -0,0 +1,19 @@
+package egor_muchin.hometask3;
+
+import java.util.Scanner;
+
+public class ThiardTask {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in); }
+        System.out.print("Input three numbers:");}
+        int aNumber = in.nextInt();
+        int bNumber = in.nextint();
+        int cNumber = in.nextint();}
+        if (aNumber > bNumber &&  bNumber > cNumber) {
+                System.out.println("Maximal number is " + aNumber);
+                } else if (bNumder > aNumber && bNumder > cNumder) {
+                System.out.println("Maximal number is " + bNumder);
+                } else if (cNumder > bNumder && cNumder > aNumber) {
+                System.out.println("Maximal number is " + cNumder);
+                }
