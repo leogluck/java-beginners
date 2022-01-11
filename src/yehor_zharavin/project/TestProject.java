16c534415fbe166078edb611124ff900c5b632eb
Yegorka <zharavin2006@gmail.com>
2022-01-11 21:08:45 +0300

    Don't work :(

---
+++ src/yehor_zharavin/project/TestProject.java
@@ -0,0 +1,23 @@
+package yehor_zharavin.project;
+
+import java.io.*;
+
+public class TestProject {
+    public static void main(String[] args) {
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
