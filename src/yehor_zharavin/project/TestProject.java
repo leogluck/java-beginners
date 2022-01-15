e6d38ca1dd965551debe28d8a3a48defd427354d
Yegorka <zharavin2006@gmail.com>
2022-01-15 14:38:23 +0300

    Don't work :(

--- src/yehor_zharavin/project/TestProject.java
+++ src/yehor_zharavin/project/TestProject.java
@@ -1,11 +1,15 @@
 package yehor_zharavin.project;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class TestProject {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int.nextInt();
         try(FileWriter writer = new FileWriter("notes3.txt", false))
         {
+
             // запись всей строки
             String text = "Hello Gold!";
             writer.write(text);
@@ -15,6 +19,8 @@ public class TestProject {
 
             writer.flush();
         }
+
+
         catch(IOException ex){
 
             System.out.println(ex.getMessage());
