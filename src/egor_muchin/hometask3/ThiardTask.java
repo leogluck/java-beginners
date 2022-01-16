0645618828f1e9458e8ec099c0f7c13815fd73b5
egor muckhin <megor2810@gmail.com>
2022-01-16 19:20:37 +0200

    ExtryRaner

--- src/egor_muchin/hometask3/ThiardTask.java
+++ src/egor_muchin/hometask3/ThiardTask.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ThiardTask {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in); }
         System.out.print("Input three numbers:");}
         int aNumber = in.nextInt();
---
+++ src/egor_muchin/hometask3/ThisrdTask2.java
@@ -0,0 +1,19 @@
+package egor_muchin.hometask3;
+
+
+import java.util.Scanner;
+
+public class ThisrdTask2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in) ;}}
+        System.out.println("введите два числа")
+        int aNumber = Nextint() ;
+        int bNumber = Nextint() ;
+        if (aNumber == 0)
+        System.out.println("эти числа не могут делится")
+                } else {
+                int ans = aNumber / bNumber;
+                if (ans == 0) {
+                System.out.println(aNumber / bNumber "не дилится");
+                }
+                }
