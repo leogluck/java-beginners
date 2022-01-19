d8fc069ecd33464a2fc96f11b1e82033ed82aae5
egor muckhin <megor2810@gmail.com>
2022-01-19 21:14:38 +0200

    Third Task

--- src/egor_muchin/hometask3/ThisrdTask2.java
+++ src/egor_muchin/hometask3/ThisrdTask2.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class ThisrdTask2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in) ;}}
-        System.out.println("введите два числа")
-        int aNumber = Nextint() ;
-        int bNumber = Nextint() ;
-        if (aNumber == 0)
-        System.out.println("эти числа не могут делится")
-                } else {
-                int ans = aNumber / bNumber;
-                if (ans == 0) {
-                System.out.println(aNumber / bNumber "не дилится");
-                }
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите два числа");
+        int aNumber = in.nextInt();
+        int bNumber = in.nextInt();
+        if (aNumber == 0) {
+            System.out.println("эти числа не могут делится");
+        } else {
+            int ans = aNumber / bNumber;
+            if (ans == 0) {
+                System.out.println(aNumber / bNumber + "не делится");
+            }
+        }
+    }
+}
