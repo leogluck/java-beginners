dcc7a0794941ba1cb5876455cf3df9095f6ce929
egor muckhin <megor2810@gmail.com>
2022-01-19 21:24:14 +0200

    Third Task

--- src/egor_muchin/hometask3/ThiardTask.java
+++ src/egor_muchin/hometask3/ThiardTask.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class ThiardTask {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in); }
-        System.out.print("Input three numbers:");}
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input three numbers:");
         int aNumber = in.nextInt();
-        int bNumber = in.nextint();
-        int cNumber = in.nextint();}
-        if (aNumber > bNumber &&  bNumber > cNumber) {
-                System.out.println("Maximal number is " + aNumber);
-                } else if (bNumder > aNumber && bNumder > cNumder) {
-                System.out.println("Maximal number is " + bNumder);
-                } else if (cNumder > bNumder && cNumder > aNumber) {
-                System.out.println("Maximal number is " + cNumder);
-                }
+        int bNumber = in.nextInt();
+        int cNumber = in.nextInt();
+
+        if (aNumber > bNumber && bNumber > cNumber) {
+            System.out.println("Maximal number is " + aNumber);
+        } else if (bNumber > aNumber && bNumber > cNumber) {
+            System.out.println("Maximal number is " + bNumber);
+        } else if (cNumber > bNumber && cNumber > aNumber) {
+            System.out.println("Maximal number is " + cNumber);
+        }
+    }
+}
