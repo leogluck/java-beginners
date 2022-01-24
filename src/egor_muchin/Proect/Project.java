cd5897447a092d718da041ce699d897ad7aa6408
muchinegor2810 <95313122+muchinegor2810@users.noreply.github.com>
2022-01-24 10:33:17 +0200

    Egor project (#83)
    * ClassProject
    
    * ClassProject

---
+++ src/egor_muchin/Proect/Project.java
@@ -0,0 +1,48 @@
+package egor_muchin.Proect;
+
+import java.util.Scanner;
+
+public class Project {
+    /**
+     * a b c d e f...
+     * f = d + e
+     */
+    public static void main(String[] args) {
+        Project project = new Project();
+        project.runProgram();
+    }
+
+    private void runProgram() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ввведите количество ступеней в лестнице:");
+        int numberOfStares = scanner.nextInt();
+        long startTimestamp = System.currentTimeMillis();
+        long numberOfCombinations = calculateCombinations(numberOfStares);
+        System.out.println("Вы можете подняться на лестницу из " + numberOfStares + " ступеней " + numberOfCombinations + " способами");
+        long endTimestamp = System.currentTimeMillis();
+        long calculationTime = endTimestamp - startTimestamp;
+        System.out.println("Время вычислений: " + calculationTime + "ms");
+    }
+
+    private long calculateCombinations(int numberOfStares) {
+        long[] dp = new long[numberOfStares + 1];
+        return recursiveCalc(dp, numberOfStares);
+    }
+
+    private long recursiveCalc(long[] dp, int stareNumber) {
+        if (stareNumber <= 2) return stareNumber;
+        if (dp[stareNumber] != 0) return dp[stareNumber];
+
+        dp[stareNumber] = recursiveCalc(dp, stareNumber - 1) + recursiveCalc(dp, stareNumber - 2);
+        return dp[stareNumber];
+    }
+
+    private void startRecursion(int i) {
+        if (i == 1000) {
+            System.out.println(i);
+            return;
+        }
+        int next = i + 1;
+        startRecursion(next);
+    }
+}
