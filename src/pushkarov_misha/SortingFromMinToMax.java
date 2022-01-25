63c746dc71cd5d8b4868bf91b39e159add7959d6
Pushkarov <wor1dofzerro@gmail.com>
2022-01-25 19:43:44 +0200

    My first telegram bot in java

---
+++ src/pushkarov_misha/SortingFromMinToMax.java
@@ -0,0 +1,27 @@
+package pushkarov_misha;
+
+public class SortingFromMinToMax {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 58, 99, 1120, 11, 5, 12, 6, 25};
+        int[] finalArray = {};
+
+        public Integer[] addElementUsingPureJava(Integer[] srcArray, int elementToAdd) {
+            Integer[] destArray = new Integer[srcArray.length+1];
+
+            for(int i = 0; i < srcArray.length; i++) {
+                destArray[i] = srcArray[i];
+            }
+
+            destArray[destArray.length - 1] = elementToAdd;
+            return destArray;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i > j) {
+
+                }
+            }
+        }
+    }
+}
