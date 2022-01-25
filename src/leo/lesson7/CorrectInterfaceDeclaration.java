4ab7fb0057e34f933eb9efb34a1550fced10ab00
Leogluck <leogluck@gmail.com>
2022-01-25 23:55:07 +0200

    Delete CorrectInterfaceDeclaration.java

--- src/leo/lesson7/CorrectInterfaceDeclaration.java
+++
@@ -1,9 +0,0 @@
-package leo.lesson7;
-
-public interface CorrectInterfaceDeclaration {
-    int getFoo();
-
-//    private void out() {
-//        System.out.println("Only private interface methods' cannot have body!!!");
-//    }
-}
