864ae5fccd1a13d466d691dfc2de85912b7f4308
Pushkarov <wor1dofzerro@gmail.com>
2022-01-25 19:28:22 +0200

    My first telegram bot in java

--- src/pushkarov_misha/TelegramBot/TelegramBotInJava.java
+++ src/pushkarov_misha/TelegramBot/TelegramBotInJava.java
@@ -1,12 +1,37 @@
 package pushkarov_misha.TelegramBot;
 
+import org.jetbrains.annotations.NotNull;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class TelegramBotInJava extends TelegramLongPollingBot {
 
     @Override
-    public void onUpdateReceived(Update update) {
+    public void onUpdateReceived(@NotNull Update update) {
+
+        String messageIn = update.getMessage().getText();
+
+        SendMessage messageOut = new SendMessage();
+
+        if (messageIn.equals("myname")) {
+            messageOut.setText(update.getMessage().getFrom().getFirstName());
+        }
+        if (messageIn.equals("mylastname")) {
+            messageOut.setText(update.getMessage().getFrom().getLastName());
+        }
+        if (messageIn.equals("myfullname")) {
+            messageOut.setText(update.getMessage().getFrom().getFirstName() + " " + update.getMessage().getFrom().getLastName());
+        }
+
+        messageOut.setChatId(update.getMessage().getChatId());
+
+        try {
+            execute(messageOut);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
     }
 
